e866d1759b846b6d58cd36166d68b058b9981944
Michael Tsitrin <michael@dymension.xyz>
2024-08-21 10:52:57 +0300

    cleaned test app

--- main.go
+++ main.go
@@ -9,15 +9,10 @@ import (
 	"github.com/dymensionxyz/cosmosclient/cosmosclient"
 )
 
-const (
-	eventStateUpdate          = "state_update.rollapp_id='rollappevm_1234-1' AND state_update.status='PENDING'"
-	eventSequencersListUpdate = "create_sequencer.rollapp_id='rollappevm_1234-1'"
-	eventRotationStarted      = "rotation_started.rollapp_id='rollappevm_1234-1'"
-)
-
 func main() {
-	// Create a new RPC client
+	var eventQuery string
 
+	// Create a new RPC client
 	options := []cosmosclient.Option{
 		cosmosclient.WithAddressPrefix("dym"),
 		cosmosclient.WithNodeAddress("http://localhost:36657"),
@@ -32,16 +27,11 @@ func main() {
 		log.Fatalf("Failed to start RPC client: %v", err)
 	}
 
-	// Subscribe to an event
-	eventQuery := ""
-	eventQuery = "state_update.rollapp_id='rollappevm_1234-1'"
-	eventQuery = "tm.event='Tx' AND message.action='/cosmos.bank.v1beta1.Msg/Send'"
-	eventQuery = "tm.event='Tx' AND message.module='bank'"
-	eventQuery = "tm.event = 'Tx'"
-	eventQuery = "tm.event = 'Tx' AND coin_received.receiver = 'dym17xpfvakm2amg962yls6f84z3kell8c5lzy0xwn'"
-	eventQuery = "coin_received.receiver = 'dym1ssx7j96d9cxestj55f05z93e36cd6nmj2rz5zv'"
-	eventQuery = eventSequencersListUpdate
+	// Subscribe to an event (examples)
 	eventQuery = "tm.event = 'NewBlock'"
+	// eventQuery = "tm.event = 'Tx'"
+	// eventQuery = "tm.event='Tx' AND message.action='/cosmos.bank.v1beta1.Msg/Send'"
+	// eventQuery = "tm.event='Tx' AND message.module='bank'"
 
 	subscription, err := rpcClient.WSEvents.Subscribe(context.Background(), fmt.Sprintf("example-client-%d", rand.Int()), eventQuery)
 	if err != nil {
@@ -50,10 +40,7 @@ func main() {
 	fmt.Println("Subscribed to event:", eventQuery)
 
 	// Listen for events
-	for {
-		select {
-		case msg := <-subscription:
-			fmt.Println("Received event:", msg)
-		}
+	for msg := range subscription {
+		fmt.Println("Received event:", msg)
 	}
 }
