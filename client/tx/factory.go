b4018b2bac132387fe850a142db5e9997b495509
Michael Tsitrin <114929630+mtsitrin@users.noreply.github.com>
2024-08-21 11:08:51 +0300

    feat: add support for 0.47 events and cleanup (#35)

--- client/tx/factory.go
+++ client/tx/factory.go
@@ -1,23 +1,16 @@
-package cosmosclient
+package tx
 
 import (
-	"context"
 	"errors"
 	"fmt"
 	"os"
 
-	"github.com/spf13/pflag"
-
 	"github.com/cosmos/cosmos-sdk/client"
-	"github.com/cosmos/cosmos-sdk/client/flags"
-	clienttx "github.com/cosmos/cosmos-sdk/client/tx"
 	"github.com/cosmos/cosmos-sdk/crypto/keyring"
 	"github.com/cosmos/cosmos-sdk/crypto/keys/secp256k1"
 	cryptotypes "github.com/cosmos/cosmos-sdk/crypto/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	"github.com/cosmos/cosmos-sdk/types/tx"
 	"github.com/cosmos/cosmos-sdk/types/tx/signing"
-	gogogrpc "github.com/gogo/protobuf/grpc"
 )
 
 // Factory defines a client transaction factory that facilitates generating and
@@ -32,6 +25,8 @@ type Factory struct {
 	timeoutHeight      uint64
 	gasAdjustment      float64
 	chainID            string
+	offline            bool
+	generateOnly       bool
 	memo               string
 	fees               sdk.Coins
 	gasPrices          sdk.DecCoins
@@ -39,85 +34,17 @@ type Factory struct {
 	simulateAndExecute bool
 }
 
-// NewFactoryCLI creates a new Factory.
-func NewFactoryCLI(clientCtx client.Context, flagSet *pflag.FlagSet) Factory {
-	signModeStr := clientCtx.SignModeStr
-
-	signMode := signing.SignMode_SIGN_MODE_UNSPECIFIED
-	switch signModeStr {
-	case flags.SignModeDirect:
-		signMode = signing.SignMode_SIGN_MODE_DIRECT
-	case flags.SignModeLegacyAminoJSON:
-		signMode = signing.SignMode_SIGN_MODE_LEGACY_AMINO_JSON
-	case flags.SignModeEIP191:
-		signMode = signing.SignMode_SIGN_MODE_EIP_191
-	}
-
-	accNum, _ := flagSet.GetUint64(flags.FlagAccountNumber)
-	accSeq, _ := flagSet.GetUint64(flags.FlagSequence)
-	gasAdj, _ := flagSet.GetFloat64(flags.FlagGasAdjustment)
-	memo, _ := flagSet.GetString(flags.FlagNote)
-	timeoutHeight, _ := flagSet.GetUint64(flags.FlagTimeoutHeight)
-
-	gasStr, _ := flagSet.GetString(flags.FlagGas)
-	gasSetting, _ := flags.ParseGasSetting(gasStr)
-
-	f := Factory{
-		txConfig:           clientCtx.TxConfig,
-		accountRetriever:   clientCtx.AccountRetriever,
-		keybase:            clientCtx.Keyring,
-		chainID:            clientCtx.ChainID,
-		gas:                gasSetting.Gas,
-		simulateAndExecute: gasSetting.Simulate,
-		accountNumber:      accNum,
-		sequence:           accSeq,
-		timeoutHeight:      timeoutHeight,
-		gasAdjustment:      gasAdj,
-		memo:               memo,
-		signMode:           signMode,
-	}
-
-	feesStr, _ := flagSet.GetString(flags.FlagFees)
-	f = f.WithFees(feesStr)
-
-	gasPricesStr, _ := flagSet.GetString(flags.FlagGasPrices)
-	f = f.WithGasPrices(gasPricesStr)
-
-	return f
-}
-
-// AccountNumber returns the account number of the Factory.
-func (f Factory) AccountNumber() uint64 { return f.accountNumber }
-
-// Sequence returns the sequence of the Factory.
-func (f Factory) Sequence() uint64 { return f.sequence }
-
-// Gas returns the gas of the Factory.
-func (f Factory) Gas() uint64 { return f.gas }
-
-// GasAdjustment returns the gas adjustment of the Factory.
-func (f Factory) GasAdjustment() float64 { return f.gasAdjustment }
-
-// Keybase returns the keybase of the Factory.
-func (f Factory) Keybase() keyring.Keyring { return f.keybase }
-
-// ChainID returns the chainID of the Factory.
-func (f Factory) ChainID() string { return f.chainID }
-
-// Memo returns the memo of the Factory.
-func (f Factory) Memo() string { return f.memo }
-
-// Fees returns the fees of the Factory.
-func (f Factory) Fees() sdk.Coins { return f.fees }
-
-// GasPrices returns the gas prices of the Factory.
-func (f Factory) GasPrices() sdk.DecCoins { return f.gasPrices }
-
-// AccountRetriever returns the account retriever of the Factory.
+func (f Factory) AccountNumber() uint64                     { return f.accountNumber }
+func (f Factory) Sequence() uint64                          { return f.sequence }
+func (f Factory) Gas() uint64                               { return f.gas }
+func (f Factory) GasAdjustment() float64                    { return f.gasAdjustment }
+func (f Factory) Keybase() keyring.Keyring                  { return f.keybase }
+func (f Factory) ChainID() string                           { return f.chainID }
+func (f Factory) Memo() string                              { return f.memo }
+func (f Factory) Fees() sdk.Coins                           { return f.fees }
+func (f Factory) GasPrices() sdk.DecCoins                   { return f.gasPrices }
 func (f Factory) AccountRetriever() client.AccountRetriever { return f.accountRetriever }
-
-// TimeoutHeight returns the timeout height of the Factory.
-func (f Factory) TimeoutHeight() uint64 { return f.timeoutHeight }
+func (f Factory) TimeoutHeight() uint64                     { return f.timeoutHeight }
 
 // SimulateAndExecute returns the option to simulate and then execute the transaction
 // using the gas from the simulation results
@@ -226,7 +153,11 @@ func (f Factory) WithTimeoutHeight(height uint64) Factory {
 // BuildUnsignedTx builds a transaction to be signed given a set of messages.
 // Once created, the fee, memo, and messages are set.
 func (f Factory) BuildUnsignedTx(msgs ...sdk.Msg) (client.TxBuilder, error) {
-	if f.chainID == "" {
+	if f.offline && f.generateOnly {
+		if f.chainID != "" {
+			return nil, fmt.Errorf("chain ID cannot be used when offline and generate-only flags are set")
+		}
+	} else if f.chainID == "" {
 		return nil, fmt.Errorf("chain ID required but not specified")
 	}
 
@@ -273,13 +204,20 @@ func (f Factory) PrintUnsignedTx(clientCtx client.Context, msgs ...sdk.Msg) erro
 			return errors.New("cannot estimate gas in offline mode")
 		}
 
-		_, adjusted, err := CalculateGas(clientCtx, f, msgs...)
+		// Prepare TxFactory with acc & seq numbers as CalculateGas requires
+		// account and sequence numbers to be set
+		preparedTxf, err := f.Prepare(clientCtx)
+		if err != nil {
+			return err
+		}
+
+		_, adjusted, err := CalculateGas(clientCtx, preparedTxf, msgs...)
 		if err != nil {
 			return err
 		}
 
 		f = f.WithGas(adjusted)
-		_, _ = fmt.Fprintf(os.Stderr, "%s\n", clienttx.GasEstimateResponse{GasEstimate: f.Gas()})
+		_, _ = fmt.Fprintf(os.Stderr, "%s\n", GasEstimateResponse{GasEstimate: f.Gas()})
 	}
 
 	unsignedTx, err := f.BuildUnsignedTx(msgs...)
@@ -304,22 +242,9 @@ func (f Factory) BuildSimTx(msgs ...sdk.Msg) ([]byte, error) {
 		return nil, err
 	}
 
-	// use the first element from the list of keys in order to generate a valid
-	// pubkey that supports multiple algorithms
-
-	var pk cryptotypes.PubKey = &secp256k1.PubKey{} // use default public key type
-
-	if f.keybase != nil {
-		infos, _ := f.keybase.List()
-		if len(infos) == 0 {
-			return nil, errors.New("cannot build signature for simulation, key infos slice is empty")
-		}
-
-		// take the first info record just for simulation purposes
-		pk, err = infos[0].GetPubKey()
-		if err != nil {
-			return nil, err
-		}
+	pk, err := f.getSimPK()
+	if err != nil {
+		return nil, err
 	}
 
 	// Create an empty signature literal as the ante handler will populate with a
@@ -336,6 +261,35 @@ func (f Factory) BuildSimTx(msgs ...sdk.Msg) ([]byte, error) {
 	}
 
 	return f.txConfig.TxEncoder()(txb.GetTx())
+}
+
+// getSimPK gets the public key to use for building a simulation tx.
+// Note, we should only check for keys in the keybase if we are in simulate and execute mode,
+// e.g. when using --gas=auto.
+// When using --dry-run, we are is simulation mode only and should not check the keybase.
+// Ref: https://github.com/cosmos/cosmos-sdk/issues/11283
+func (f Factory) getSimPK() (cryptotypes.PubKey, error) {
+	var (
+		ok bool
+		pk cryptotypes.PubKey = &secp256k1.PubKey{} // use default public key type
+	)
+
+	// Use the first element from the list of keys in order to generate a valid
+	// pubkey that supports multiple algorithms.
+	if f.simulateAndExecute && f.keybase != nil {
+		records, _ := f.keybase.List()
+		if len(records) == 0 {
+			return nil, errors.New("cannot build signature for simulation, key records slice is empty")
+		}
+
+		// take the first record just for simulation purposes
+		pk, ok = records[0].PubKey.GetCachedValue().(cryptotypes.PubKey)
+		if !ok {
+			return nil, errors.New("cannot build signature for simulation, failed to convert proto Any to public key")
+		}
+	}
+
+	return pk, nil
 }
 
 // Prepare ensures the account defined by ctx.GetFromAddress() exists and
@@ -368,32 +322,4 @@ func (f Factory) Prepare(clientCtx client.Context) (Factory, error) {
 	}
 
 	return fc, nil
-}
-
-// CalculateGas simulates the execution of a transaction and returns the
-// simulation response obtained by the query and the adjusted gas amount.
-func CalculateGas(
-	clientCtx gogogrpc.ClientConn, txf Factory, msgs ...sdk.Msg,
-) (*tx.SimulateResponse, uint64, error) {
-	txBytes, err := BuildSimTx(txf, msgs...)
-	if err != nil {
-		return nil, 0, err
-	}
-
-	txSvcClient := tx.NewServiceClient(clientCtx)
-	simRes, err := txSvcClient.Simulate(context.Background(), &tx.SimulateRequest{
-		TxBytes: txBytes,
-	})
-	if err != nil {
-		return nil, 0, err
-	}
-
-	return simRes, uint64(txf.GasAdjustment() * float64(simRes.GasInfo.GasUsed)), nil
-}
-
-// BuildSimTx creates an unsigned tx with an empty single signature and returns
-// the encoded transaction or an error if the unsigned transaction cannot be
-// built.
-func BuildSimTx(txf Factory, msgs ...sdk.Msg) ([]byte, error) {
-	return txf.BuildSimTx(msgs...)
 }
--- go.mod
+++ go.mod
@@ -5,16 +5,15 @@ go 1.18
 require (
 	github.com/cenkalti/backoff v2.2.1+incompatible
 	github.com/cosmos/cosmos-sdk v0.46.13
+	github.com/evmos/evmos/v12 v12.1.6
 	github.com/gogo/protobuf v1.3.3
-	github.com/ignite/cli v0.25.2
+	github.com/ignite/cli v0.26.1
 	github.com/pkg/errors v0.9.1
-	github.com/spf13/pflag v1.0.5
 	github.com/tendermint/tendermint v0.34.28
-	google.golang.org/grpc v1.54.0
+	google.golang.org/grpc v1.62.1
 )
 
 require (
-	cloud.google.com/go/storage v1.28.1 // indirect
 	cosmossdk.io/errors v1.0.0-beta.7 // indirect
 	cosmossdk.io/math v1.0.0-rc.0 // indirect
 	filippo.io/edwards25519 v1.0.0-rc.1 // indirect
@@ -24,14 +23,18 @@ require (
 	github.com/armon/go-metrics v0.4.1 // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/bgentry/speakeasy v0.1.1-0.20220910012023-760eaf8b6816 // indirect
-	github.com/blang/semver v3.5.1+incompatible // indirect
+	github.com/blang/semver/v4 v4.0.0 // indirect
+	github.com/btcsuite/btcd v0.22.2 // indirect
 	github.com/btcsuite/btcd/btcec/v2 v2.3.2 // indirect
+	github.com/btcsuite/btcd/chaincfg/chainhash v1.0.1 // indirect
+	github.com/btcsuite/btcutil v1.0.3-0.20201208143702-a53e38424cce // indirect
 	github.com/cespare/xxhash v1.1.0 // indirect
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/confio/ics23/go v0.9.0 // indirect
 	github.com/cosmos/btcutil v1.0.5 // indirect
 	github.com/cosmos/cosmos-proto v1.0.0-beta.3 // indirect
 	github.com/cosmos/go-bip39 v1.0.0 // indirect
+	github.com/cosmos/gogoproto v1.5.0 // indirect
 	github.com/cosmos/gorocksdb v1.2.0 // indirect
 	github.com/cosmos/iavl v0.19.6 // indirect
 	github.com/cosmos/ledger-cosmos-go v0.12.2 // indirect
@@ -43,18 +46,18 @@ require (
 	github.com/dgryski/go-farm v0.0.0-20200201041132-a6ae2369ad13 // indirect
 	github.com/dustin/go-humanize v1.0.1-0.20200219035652-afde56e7acac // indirect
 	github.com/dvsekhvalnov/jose2go v1.5.0 // indirect
-	github.com/evmos/evmos/v12 v12.1.6 // indirect
+	github.com/ethereum/go-ethereum v1.10.26 // indirect
 	github.com/fsnotify/fsnotify v1.6.0 // indirect
 	github.com/ghodss/yaml v1.0.0 // indirect
 	github.com/go-kit/kit v0.12.0 // indirect
 	github.com/go-kit/log v0.2.1 // indirect
 	github.com/go-logfmt/logfmt v0.5.1 // indirect
 	github.com/godbus/dbus v0.0.0-20190726142602-4481cbc300e2 // indirect
-	github.com/golang/glog v1.0.0 // indirect
+	github.com/golang/glog v1.2.0 // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/golang/snappy v0.0.4 // indirect
 	github.com/google/btree v1.1.2 // indirect
-	github.com/google/gofuzz v1.2.0 // indirect
+	github.com/google/go-cmp v0.6.0 // indirect
 	github.com/gorilla/mux v1.8.0 // indirect
 	github.com/gorilla/websocket v1.5.0 // indirect
 	github.com/grpc-ecosystem/go-grpc-middleware v1.3.0 // indirect
@@ -76,8 +79,6 @@ require (
 	github.com/mimoo/StrobeGo v0.0.0-20210601165009-122bf33a46e0 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/mtibben/percent v0.2.1 // indirect
-	github.com/onsi/gomega v1.27.2 // indirect
-	github.com/pelletier/go-toml v1.9.5 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.7 // indirect
 	github.com/petermattis/goid v0.0.0-20230317030725-371a4b8eda08 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
@@ -92,26 +93,34 @@ require (
 	github.com/spf13/cast v1.5.0 // indirect
 	github.com/spf13/cobra v1.6.1 // indirect
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/spf13/viper v1.15.0 // indirect
-	github.com/stretchr/testify v1.8.2 // indirect
+	github.com/stretchr/testify v1.8.4 // indirect
 	github.com/subosito/gotenv v1.4.2 // indirect
 	github.com/syndtr/goleveldb v1.0.1-0.20220721030215-126854af5e6d // indirect
 	github.com/tendermint/go-amino v0.16.0 // indirect
 	github.com/tendermint/tm-db v0.6.7 // indirect
 	github.com/tidwall/btree v1.5.0 // indirect
+	github.com/tidwall/gjson v1.14.4 // indirect
+	github.com/tidwall/match v1.1.1 // indirect
+	github.com/tidwall/pretty v1.2.0 // indirect
+	github.com/tidwall/sjson v1.2.5 // indirect
+	github.com/tyler-smith/go-bip39 v1.1.0 // indirect
 	github.com/zondax/hid v0.9.1 // indirect
 	github.com/zondax/ledger-go v0.14.1 // indirect
 	go.etcd.io/bbolt v1.3.6 // indirect
-	golang.org/x/crypto v0.7.0 // indirect
-	golang.org/x/exp v0.0.0-20230310171629-522b1b587ee0 // indirect
-	golang.org/x/mod v0.8.0 // indirect
-	golang.org/x/net v0.9.0 // indirect
-	golang.org/x/sync v0.1.0 // indirect
-	golang.org/x/sys v0.7.0 // indirect
-	golang.org/x/term v0.7.0 // indirect
-	golang.org/x/text v0.9.0 // indirect
-	google.golang.org/genproto v0.0.0-20230306155012-7f2fa6fef1f4 // indirect
-	google.golang.org/protobuf v1.30.0 // indirect
+	golang.org/x/crypto v0.18.0 // indirect
+	golang.org/x/exp v0.0.0-20231006140011-7918f672742d // indirect
+	golang.org/x/mod v0.13.0 // indirect
+	golang.org/x/net v0.20.0 // indirect
+	golang.org/x/sync v0.6.0 // indirect
+	golang.org/x/sys v0.16.0 // indirect
+	golang.org/x/term v0.16.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+	google.golang.org/genproto v0.0.0-20240123012728-ef4313101c80 // indirect
+	google.golang.org/genproto/googleapis/api v0.0.0-20240123012728-ef4313101c80 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240123012728-ef4313101c80 // indirect
+	google.golang.org/protobuf v1.32.0 // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
@@ -120,5 +129,5 @@ require (
 
 replace (
 	github.com/gogo/protobuf => github.com/regen-network/protobuf v1.3.3-alpha.regen.1
-	github.com/tendermint/tendermint => github.com/informalsystems/tendermint v0.34.26
+	github.com/tendermint/tendermint => github.com/dymensionxyz/cometbft v0.34.30-0.20240820154924-2f433c5fa40d
 )
